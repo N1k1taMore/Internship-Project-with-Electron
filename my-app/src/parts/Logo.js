6f3a9bda2f19b3dabe81365e6f56ecc153e501cf
Manav2031 <manavmehta2031@gmail.com>
2024-11-12 22:55:20 +0530

    Updated code

--- my-app/src/parts/Logo.js
+++ my-app/src/parts/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import LogoImage from '../assets/logo1.png'
+import LogoImage from '../assets/logo.png'
 
 const LogoText = styled.h1`
   font-family: 'Akaya Telivigala', cursive;
